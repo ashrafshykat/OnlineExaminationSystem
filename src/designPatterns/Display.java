7ecb4ab8e345bcbec03ed125a543b644ac8fec5a
ashrafshykat <ashrafshykat1@gmail.com>
2024-11-03 01:32:52 +0600

    Dependency Injection

--- src/designPatterns/Display.java
+++ src/designPatterns/Display.java
@@ -1,15 +1,18 @@
 package designPatterns;
 
 public class Display implements IDisplay, IObserver {
-
+	Last15min minute;
+	public Display(Last15min minute) {
+		this.minute = minute;
+	}
 	@Override
 	public void update() {
-
+		System.out.println("15 minutes remaining");
 	}
 
 	@Override
 	public void display() {
-
+		System.out.println("15 minutes remaining");
 	}
 
 }
